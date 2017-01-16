f0ab59cda143633abaede65a2f48e19f68e9bd31
zhangchao <zhangchao@plu.cn>
2017-01-16 09:41:01 +0800

    去除注册界面

--- controllers/user/user.go
+++ controllers/user/user.go
@@ -12,9 +12,9 @@ func (this *User) Login() {
 	this.Data["vueVersion"] = 1
 }
 
-func (this *User) Register() {
-	this.Data["vueVersion"] = 1
-}
+// func (this *User) Register() {
+// 	this.Data["vueVersion"] = 1
+// }
 
 func (this *User) Forgetpassword() {
 	this.Data["vueVersion"] = 1
